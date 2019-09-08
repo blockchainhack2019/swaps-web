46ba87397aaf2c281156058b026f905f6134fc8c
Aleksey Bykhun <theisease@gmail.com>
2019-09-08 11:18:42 +0300

    fix web3 import

--- babel.config.js
+++ babel.config.js
@@ -20,6 +20,7 @@ const config = {
     '@babel/plugin-transform-destructuring',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-runtime',
   ],
 }
 
